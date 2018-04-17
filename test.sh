93a10ecb940960958c70117704bb29f7d811ded1
SantiagoLazzari <slazzari@fi.uba.ar>
2018-04-17 00:33:31 -0300

    Added validation again

--- test.sh
+++ test.sh
@@ -15,21 +15,15 @@ function validation {
 echo "Default fractal test"
 
 ./tp0 -o $AUX_IMAGE
-
-RESULT=$(diff out/julia.pgm $AUX_IMAGE)
-
+RESULT=$(diff julia.pgm $AUX_IMAGE)
 validation $RESULT
 
 echo "Seed  0.279+0 i test"
 
 ./tp0 −s 0.279+0i -o $AUX_IMAGE
-
 RESULT=$(diff out/julia_seed.pgm $AUX_IMAGE)
+validation $RESULT
 
-if [[ -z $RESULT ]]; then
- echo "Test OK"
-else
- echo "Test Fail"
-fi
+
 
 rm $AUX_IMAGE
